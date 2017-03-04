8d0ed677a80756b0fb76fdbd07fa48fe1f41d44a
Malte <malte.schremmer@gmail.com>
2017-03-04 22:02:55 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_xjrH3gSImzRKqIkhRAqpAAMjH387yaBWfGbidkKtqM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Uk34qKL3uI1DRjHcFAETIj9dXJ0QEiWZFtHHBHY_SRo/edit#gid=0';
